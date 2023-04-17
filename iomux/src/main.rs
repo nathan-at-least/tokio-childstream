e69b2709495bc81149be78cb92784793ea9b4210
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-16 19:34:34 -0700

    Initial PoC implementation of `iomux`.

--- iomux/src/main.rs
+++ iomux/src/main.rs
@@ -1,9 +1,40 @@
 mod options;
 
 use self::options::Options;
+use futures::StreamExt;
+use tokio_childstream::{spawn_stream, ChildItem::*};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     let opts = Options::parse()?;
-    todo!("{:#?}", opts.subcommands);
+
+    let mut streams = vec![];
+    for (i, mut cmd) in opts.subcommands.into_iter().enumerate() {
+        println!("{}I: Spawning {:?}", i, cmd.as_std());
+        let stream = spawn_stream(&mut cmd)?;
+        streams.push(stream.map(move |event| (i, event)));
+    }
+
+    let mut events = futures::stream::select_all(streams);
+    while let Some((i, evres)) = events.next().await {
+        match evres {
+            Ok(Stdout(bytes)) => {
+                println!("{}O: {}", i, String::from_utf8_lossy(bytes.as_ref()));
+            }
+            Ok(Stderr(bytes)) => {
+                println!("{}E: {}", i, String::from_utf8_lossy(bytes.as_ref()));
+            }
+            Ok(Exit(status)) => {
+                if let Some(code) = status.code() {
+                    println!("{}X: exit {}", i, code);
+                } else {
+                    println!("{}X: exit ???", i);
+                }
+            }
+            Err(e) => {
+                println!("{}!: {}", i, e);
+            }
+        }
+    }
+    Ok(())
 }
