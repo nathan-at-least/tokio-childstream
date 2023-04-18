815e428ca6a14d45d263585e316161e142070edd
Nate Wilcox <nathan+dev@electriccoin.co>
2023-04-18 08:53:54 -0700

    Factor out duplicate code in `ChildStream::from::<Child>()`.

--- tokio-childstream/src/lib.rs
+++ tokio-childstream/src/lib.rs
@@ -6,7 +6,7 @@ mod stream;
 
 pub use self::commandext::CommandExt;
 pub use self::item::{ByteSource, ChildItem};
-pub use self::stream::ChildStream;
+pub use self::stream::{ChildStream, StreamItem};
 
 #[cfg(test)]
 mod tests;
--- tokio-childstream/src/stream/fromchild.rs
+++ tokio-childstream/src/stream/fromchild.rs
@@ -1,45 +1,20 @@
 use super::ChildStream;
-use super::StreamItem;
-use futures::Stream;
+use crate::{ByteSource, StreamItem};
+use futures::channel::mpsc;
+use futures::{Stream, StreamExt};
 use std::pin::Pin;
+use tokio::io::AsyncRead;
 use tokio::process::Child;
 
 impl From<Child> for ChildStream {
     fn from(mut child: Child) -> Self {
         use crate::{ByteSource::*, ChildItem::*};
-        use futures::channel::mpsc;
-        use futures::StreamExt;
-        use tokio_util::io::ReaderStream;
 
         let id = child.id().unwrap();
-        let optout = child.stdout.take();
-        let opterr = child.stderr.take();
-
         let (sender, receiver) = mpsc::unbounded();
 
-        if let Some(stdout) = optout {
-            let outsender = sender.clone();
-            tokio::task::spawn(async move {
-                let mut stream = ReaderStream::new(stdout);
-                while let Some(bytesres) = stream.next().await {
-                    outsender
-                        .unbounded_send(bytesres.map(|b| Bytes(Stdout, b)))
-                        .unwrap();
-                }
-            });
-        }
-
-        if let Some(stderr) = opterr {
-            let errsender = sender;
-            tokio::task::spawn(async move {
-                let mut stream = ReaderStream::new(stderr);
-                while let Some(bytesres) = stream.next().await {
-                    errsender
-                        .unbounded_send(bytesres.map(|b| Bytes(Stderr, b)))
-                        .unwrap();
-                }
-            });
-        }
+        create_optional_send_task(sender.clone(), child.stdout.take(), Stdout);
+        create_optional_send_task(sender, child.stderr.take(), Stderr);
 
         let exitstream: Pin<Box<dyn Stream<Item = StreamItem>>> =
             Box::pin(futures::stream::once(async move {
@@ -49,5 +24,27 @@ impl From<Child> for ChildStream {
         let stream = receiver.chain(exitstream);
 
         ChildStream { id, stream }
+    }
+}
+
+fn create_optional_send_task<R>(
+    sender: mpsc::UnboundedSender<StreamItem>,
+    optout: Option<R>,
+    source: ByteSource,
+) where
+    R: AsyncRead + Unpin + Send + 'static,
+{
+    use crate::ChildItem::Bytes;
+    use tokio_util::io::ReaderStream;
+
+    if let Some(out) = optout {
+        tokio::task::spawn(async move {
+            let mut stream = ReaderStream::new(out);
+            while let Some(bytesres) = stream.next().await {
+                sender
+                    .unbounded_send(bytesres.map(|b| Bytes(source, b)))
+                    .unwrap();
+            }
+        });
     }
 }
