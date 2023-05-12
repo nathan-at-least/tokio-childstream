4867db6bb8fffb9fb1103419c13ad6bc51b3a67a
Nate Wilcox <nathan+dev@electriccoin.co>
2023-05-11 22:38:45 -0700

    Fix /// vs //! mistake

--- bytelinebuf/src/lib.rs
+++ bytelinebuf/src/lib.rs
@@ -1,4 +1,4 @@
-/// `Iterator` and `Stream` types for splitting bytes on `'\n'`
+//! `Iterator` and `Stream` types for splitting bytes on `'\n'`
 mod buf;
 
 pub use self::buf::{ByteLineBuf, DrainLines};
