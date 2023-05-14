2b33fdcea4365c646aca1af5c209475e51bdc42e
Nate Wilcox <nathan+dev@electriccoin.co>
2023-05-14 06:32:14 -0700

    Split out `exoshell-command`.

--- exoshell/main/src/runner.rs
+++ exoshell/main/src/runner.rs
@@ -21,8 +21,8 @@ impl Runner {
             self.evs.send_quit()?;
             Ok(())
         } else {
-            use crate::cmd::Command;
             use crate::event::ChildEvent;
+            use exoshell_command::Command;
             use futures::stream::StreamExt;
 
             let cmd: Command = cmdtext.parse()?;
